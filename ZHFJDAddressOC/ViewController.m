fbca0649a0b9a338f786923ae0187343b45b38a4
张海峰 <991150443@qq.com>
2018-05-10 17:10:21 +0800

    Update ViewController.m

--- ZHFJDAddressOC/ViewController.m
+++ ZHFJDAddressOC/ViewController.m
@@ -4,14 +4,17 @@
 //
 //  Created by 张海峰 on 2017/12/18.
 //  Copyright © 2017年 张海峰. All rights reserved.
-//写过Swift版本之后，有读者需要OC版本，现写下，希望能为你们提供帮助。
+// 这是一个自定义仿京东地址选择器。OC版本，（保证集成成功，有不懂的地方可加QQ：991150443 进行讨论。）
+// Swift版本地址：https://github.com/FighterLightning/ZHFJDAddress.git
 /*该demo的使用须知:
- 1.把Address文件拖进项目
+ 1.下载该demo。把Address文件拖进项目（里面有一个View（主要），四个model（一个网络，剩下省市区））
  2.pod 'AFNetworking'//网络请求
  pod 'YYModel' //字典转模型
  3.把以下代码添加进自己的控制器方可使用,注意顺序，网络请求看ZHFAddTitleAddressView.m头部注释根据需求进行修改
  4.如果感觉有帮助，不要吝啬你的星星哦！
+ 该demo地址：https://github.com/FighterLightning/ZHFJDAddressOC.git
  */
+
 #define screen_width [UIScreen mainScreen].bounds.size.width
 #define screen_height [UIScreen mainScreen].bounds.size.height
 #import "ViewController.h"
